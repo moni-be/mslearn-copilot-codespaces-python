54f3d21e22c6e0d1cc68c178cc1b743f31a60820
moni-be <19931080+moni-be@users.noreply.github.com>
2025-03-11 18:54:01 +0000

    working & adding code with Copilote

--- webapp/main.py
+++ webapp/main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
+import hashlib
 
 current_dir = dirname(abspath(__file__))
 static_path = join(current_dir, "static")
@@ -27,11 +28,29 @@ def root():
 @app.post('/generate')
 def generate(body: Body):
     """
-    Generate a pseudo-random token ID of twenty characters by default. Example POST request body:
-
+    Generate a pseudo-random token ID of twenty characters
+    by default.
+    Example POST request body:
     {
         "length": 20
     }
     """
     string = base64.b64encode(os.urandom(64))[:body.length].decode('utf-8')
     return {'token': string}
+
+
+class Text(BaseModel):
+    text: str
+
+
+@app.post('/checksum')
+def checksum(text: Text):
+    """
+    Calculate the checksum of the provided text. Example POST request body:
+
+    {
+        "text": "example text"
+    }
+    """
+    checksum_value = hashlib.md5(text.text.encode('utf-8')).hexdigest()
+    return {'checksum': checksum_value}
